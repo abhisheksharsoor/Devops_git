3f9590d0a8d9272a7fb6b6996f9a7c0678f1d41b
abhisheksharsoor <53474203+abhisheksharsoor@users.noreply.github.com>
2026-06-30 23:04:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = var.rg_name
-  location = var.location
-}
-variable "rg_name" {
-  type        = string
-  description = "Name of the resource group"
-}
-
-variable "location" {
-  type        = string
-  description = "Azure region"
-}
-output "rg_name" {
-  value = azurerm_resource_group.rg.name
-}
-
-output "rg_location" {
-  value = azurerm_resource_group.rg.location
+  name     = "demo-resource-group"
+  location = "eastus"
 }
